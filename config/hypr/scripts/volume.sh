3716b42fda954d31c120519b02ebd9cdbab8b352
kaii-lb <mouhanadmasre164@gmail.com>
2023-04-12 22:26:47 +0300

    new hypr style and improved waybar and tools

---
+++ config/hypr/scripts/volume.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+if [[ $1 == "increase" ]]; then
+	ismuted=$(pamixer --get-mute)
+
+	if [ "$ismuted" = true ]; then
+		pamixer --unmute 
+	fi
+
+	if [[ -z $2 ]]; then
+		pamixer -i 5
+	else
+		pamixer -i $2
+	fi
+	
+	canberra-gtk-play -i audio-volume-change -d "changeVolume"
+
+	echo increased
+	exit 0
+elif [[ $1 == "decrease" ]]; then
+	ismuted=$(pamixer --get-mute)
+
+	if [ "$ismuted" = true ]; then
+			pamixer --unmute
+	fi
+
+	if [[ -z $2 ]]; then
+		pamixer -d 5
+	else	
+		pamixer -d $2
+	fi
+	
+	canberra-gtk-play -i audio-volume-change -d "changeVolume"
+
+	echo decreased
+	exit 0
+elif [[ $1 == "toggle-mute" ]]; then
+	ismuted=$(pamixer --get-mute)
+
+	if [ "$ismuted" = false ]; then
+		pamixer --mute 
+	else
+		pamixer --unmute
+	fi
+
+	echo toggled mute
+	exit 0
+else
+	echo -e "Usage: volume [option] [value]"
+	echo -e "\nOptions: "
+	echo -e "\n    increase: increases the volume by specified value (5% if no value is provided)"
+	echo -e "\n    decrease: decreases the volume by specified value (5% if no value is provided)"
+	echo -e "\n    toggle-mute: ...toggles muting, pretty self explanatory"
+fi
--- config/waybar/scripts/get-media-player.sh
+++ config/waybar/scripts/get-media-player.sh
@@ -2,13 +2,25 @@
 
 player=$(playerctl --player=io,com,spotify,firefox,%any metadata --format '{{playerName}}')
 
-if [[ $player -eq "io" ]]; then
+if [[ $player =~ "io" ]]; then
 	player_name=amberol
-elif [[ $player -eq "com" ]]; then
+elif [[ $player =~ "com" ]]; then
 	player_name=g4music
 else
 	player_name=$player
 fi
 
-exec $player_name
+is_running=$(pgrep $player_name > /dev/null && echo 0)
+
+if [[ $is_running -eq 0 ]];then
+	id=$(hyprctl clients -j | jq -r '.[] | select(.class == "io.bassi.Amberol").workspace["id"]')
+	class=$(hyprctl clients -j | jq -r '.[] | select(.class == "io.bassi.Amberol").class')
+	hyprctl dispatch workspace $id && echo "Moved to ${id}"
+	hyprctl dispatch focuswindow $class && echo "Focused ${class}"
+	#hyprctl dispatch focuswindow "io.bassi.Amberol"
+else
+	$player_name
+fi
+
+#exec $player_name
 
