4a6bacf07bf1c7aa39100fa26141c595226057c7
kaii <imkaiilb@gmail.com>
2023-07-16 21:46:09 +0300

    fixed powermenu confirm issue

--- config/waybar/scripts/get-media-player.sh
+++ config/waybar/scripts/get-media-player.sh
@@ -10,7 +10,7 @@ elif [[ $player =~ "com" ]]; then
 	class=$(hyprctl clients -j | jq -r '.[] | select(.class | startswith("com")).class')
 	player_name=$(echo $class | grep -oE '[^. ]+$' | awk '{print tolower($0)}')
 else
-	class=$(hyprctl clients -j | jq -r '.[] | select(.class | startswith("com")).class')
+	class=$(hyprctl clients -j | jq --arg name $player -r '.[] | select(.class | ascii_downcase | startswith("$ARGS.named.name")).class')
 	player_name=$player
 fi
 
@@ -19,9 +19,9 @@ is_running=$(pgrep $player_name > /dev/null && echo 0)
 echo $player
 
 if [[ $is_running -eq 0 ]];then
-	id=$(hyprctl clients -j | jq --arg name $player -r '.[] | select(.class | startswith($ARGS.named.name)).workspace["id"]')
+	id=$(hyprctl clients -j | jq --arg name $player -r '.[] | select(.class | ascii_downcase | contains($ARGS.named.name)).workspace["id"]')
 	echo "id is " $id
-	class=$(hyprctl clients -j | jq --arg name $player -r '.[] | select(.class | startswith($ARGS.named.name)).class')
+	class=$(hyprctl clients -j | jq --arg name $player -r '.[] | select(.class | ascii_downcase | contains($ARGS.named.name)).class')
 	hyprctl dispatch workspace $id && echo "Moved to ${id}"
 	hyprctl dispatch focuswindow $class && echo "Focused ${class}"
 else
