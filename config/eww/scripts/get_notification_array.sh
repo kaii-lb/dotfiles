21e9ad252461e84bb7a2ca017db9715aac9edb4f
kaii_lb <imkaiilb@gmail.com>
2025-05-22 17:40:09 +0300

    added volume osd

--- config/eww/scripts/get_notification_array.sh
+++ config/eww/scripts/get_notification_array.sh
@@ -4,6 +4,10 @@ count=$(head -n 2 ~/.cache/notifications.json | grep count | tr -d '["count:", "
 
 string=""
 
+if [[ $count -ge 100 ]]; then
+	count=100
+fi
+
 for i in $(eval echo "{0..${count}}"); do
 	if [[ 0 == $i ]]; then
 		string="${i}"
--- config/eww/scripts/get_volume_icon.sh
+++ config/eww/scripts/get_volume_icon.sh
@@ -2,12 +2,22 @@
 
 volume=$(pamixer --get-volume)
 
+echo $volume >> /tmp/current_volume
+
 if [[ $volume -eq 0 ]]; then
-	echo "/home/kaii/.config/eww/assets/volume_mute.svg" >> /tmp/current_volume
+	echo "/home/kaii/.config/eww/assets/volume_mute.svg" >> /tmp/current_volume_icon
 elif [[ $volume -lt 33 ]]; then
-	echo "/home/kaii/.config/eww/assets/volume_low.svg" >> /tmp/current_volume
+	echo "/home/kaii/.config/eww/assets/volume_low.svg" >> /tmp/current_volume_icon
 elif [[ $volume -lt 67 ]]; then
-	echo "/home/kaii/.config/eww/assets/volume_mid.svg" >> /tmp/current_volume
+	echo "/home/kaii/.config/eww/assets/volume_mid.svg" >> /tmp/current_volume_icon
 else
-	echo "/home/kaii/.config/eww/assets/volume_high.svg" >> /tmp/current_volume
+	echo "/home/kaii/.config/eww/assets/volume_high.svg" >> /tmp/current_volume_icon
+fi
+
+eww update open_volume_osd=true
+
+sleep 5
+eww_volume=$(eww get volume)
+if [[ eww_volume -eq $volume ]]; then
+	eww update open_volume_osd=false
 fi
